d5a2efddb33f1ac9369debdca985f2e90f4d1c77
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2026-03-01 23:53:10 -0600

    Refactor

--- lib/ui/play_bar.py
+++ lib/ui/play_bar.py
@@ -1,3 +1,6 @@
+from lib.ui.helpers import toggle_css
+from lib.ui.helpers import toggle_icon
+from lib.ui.helpers import format_time
 from lib.state.player_state import PlayState
 from typing import Optional
 from lib.sys.MPRISController import MPRISController
@@ -6,32 +9,6 @@ import logging
 from gi.repository import Gtk, GLib, Adw, Pango, Gst
 from reactivex import combine_latest
 from lib.state.player_state import PlayerState
-
-
-def toggle_css(widget: Gtk.Widget, class_name: str, active: bool) -> None:
-    if active:
-        widget.add_css_class(class_name)
-    else:
-        widget.remove_css_class(class_name)
-
-
-def toggle_icon(widget: Gtk.Button, active: bool, on_icon: str, off_icon: str) -> None:
-    widget.set_icon_name(on_icon if active else off_icon)
-
-
-def format_time(ns: int) -> str:
-    if ns == 0:
-        return "0:00"
-    if ns < 0:
-        return "N/A"
-    seconds = ns // 1_000_000_000
-    hours = seconds // 3600
-    minutes = (seconds % 3600) // 60
-    secs = seconds % 60
-    if hours > 0:
-        return f"{hours}:{minutes:02d}:{secs:02d}"
-    else:
-        return f"{minutes}:{secs:02d}"
 
 
 # --- Functional Components ---
